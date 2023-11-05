c87c7617208ce3fcb1b1d3b80086425f2189dd1c
yeetbix529 <Josh.sul529@gmail.com>
2023-11-05 14:19:05 +1100

    updated open ide cmds
    minor:
    - removed invalid .png image

--- OpenSikulixide_IDE.bat
+++ OpenSikulixide_IDE.bat
@@ -1,1 +1,2 @@
-java -jar C:\Users\joshs\Desktop\Sikulixide\sikulixide-2.0.5.jar
+cd C:\Users\joshs\Documents\jdk-20.0.2\bin
+java -jar C:\Users\joshs\Documents\GitHub\bountyofone_automation\sikulixide-2.0.5.jar
